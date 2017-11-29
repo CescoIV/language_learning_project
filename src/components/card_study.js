33aff061980973ed4e9ed51209e0ce40c40e5f88
Cesco <crordaz@gmail.com>
2017-11-29 15:37:45 -0600

    all functionality ready, page redesign necessary

--- src/components/card_study.js
+++ src/components/card_study.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Card from './card.js';
 import Quiz from './quiz.js';
+import Search from './search.js';
 import axios from 'axios';
 import './card_study.css';
 
@@ -15,6 +16,7 @@ class CardStudy extends Component {
     this._flashCards = this._flashCards.bind(this);
     this._quizGenerator = this._quizGenerator.bind(this);
     this._getData = this._getData.bind(this);
+    this._searchComp = this._searchComp.bind(this);
   }
 
   _flashCards(){
@@ -24,6 +26,12 @@ class CardStudy extends Component {
     this.setState({
       message: (<div className='read'><h1>Study Time!</h1><p>Take your time to study some words, five are selected at random each time:</p></div>),
       display: dataCards
+    })
+  }
+  _searchComp(){
+    this.setState({
+      message:(<div className='read'><h1>Search for a word!</h1></div>),
+      display: (<Search data={this.state.data}/>),
     })
   }
   _shuffle(words){
@@ -66,6 +74,7 @@ class CardStudy extends Component {
           <nav className='nav_bar'>
             <div onClick={this._flashCards}><p title="study flash cards"className='nav_button s'>S</p></div>
             <div onClick={this._quizGenerator}><p title="Quiz me!"className='nav_button q'>Q</p></div>
+            <div onClick={this._searchComp}><p title='Search words'className='nav_button'>Se</p></div>
           </nav>
         </div>
         <div className='display'>{this.state.message}{this.state.display}</div>
---
+++ src/components/search.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      query : '',
+      data: null,
+      display: null,
+    }
+    this._updateQuery = this._updateQuery.bind(this);
+    this._handleSearch = this._handleSearch.bind(this);
+    this._showWordInfo = this._showWordInfo.bind(this);
+  }
+  _updateQuery(e){
+    e.preventDefault();
+    this.setState({
+      query: e.target.value,
+      display: null,
+    })
+  }
+  _handleSearch(e){
+    //may need to break into divide search as data gets bigger
+    e.preventDefault();
+    let found = {};
+    for(let obj of this.props.data){
+      let n = obj.word_native.toLowerCase();
+      let en = obj.word_english.toLowerCase();
+      let current = this.state.query;
+      if(n === current || en === current){
+        found = obj;
+        break;
+      }
+    }
+    this._showWordInfo(found);
+  }
+  _showWordInfo(obj){
+    let resp = obj.correct_responses.map((el)=>{
+      if(el !== obj.correct_responses[obj.correct_responses.length-1]){
+        el += ", "
+      }
+      return el;
+    })
+    console.log(resp)
+    let stuff = (
+      <div>
+        <h3>Here is what I know about this word</h3>
+        <p>In Nahuatl, it is: <span>{obj.word_native}</span></p>
+        <p>It Translates to <span>{obj.word_english}</span> in English</p>
+        <p>Here are some possible translations:</p>
+        {resp}
+      </div>
+      )
+    this.setState({
+      display: stuff
+    })
+  }
+  render() {                      
+    return (
+      <div className="App">
+        <p>hello</p>
+        <p>Show all words? click moi</p>
+        <form onSubmit={this._handleSearch}>
+          <span>Search: </span><input onChange={this._updateQuery}type='text'></input>
+        </form>
+        {this.state.display}
+      </div>
+    );
+  }
+}
+
+export default Search;
